925ad843af71e6225cc67f51001abf2d4cac746a
alexander-sysuiev <alexander.sysuiev@gmail.com>
2018-05-11 21:46:54 +0300

    Add rspec tests

---
+++ spec/models/url_relation_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe UrlRelation do
+
+  it 'creates successfully' do
+    expect { UrlRelation.create!(short_version: 'aaa', full_version: 'ddddd') }.to_not raise_error
+  end
+
+  it 'raise validation error in case of empty short version field' do
+    expect { UrlRelation.create!(short_version: nil, full_version: 'ddddd') }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'raise validation error in case of empty full version field' do
+    expect { UrlRelation.create!(short_version: 'aaa', full_version: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+end
