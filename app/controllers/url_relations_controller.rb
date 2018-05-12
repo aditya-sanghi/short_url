cf01f3109cb4cca412a8af7559b8829ea22a09d8
alexander-sysuiev <alexander.sysuiev@gmail.com>
2018-05-12 12:09:56 +0300

    Show result after submiting url

--- app/controllers/url_relations_controller.rb
+++ app/controllers/url_relations_controller.rb
@@ -1,13 +1,13 @@
 class UrlRelationsController < ApplicationController
   def new
-    @url_relation = UrlRelation.new
+    @url_relation = UrlRelation.find_or_initialize_by(full_version: params[:full_url])
   end
 
   def create
-    @url_relation = UrlRelation.new(url_relation_params)
+    @url_relation = UrlRelation.find_or_initialize_by(url_relation_params)
 
     if @url_relation.save
-      redirect_to new_url_relation_path
+      redirect_to new_url_relation_path(full_url: @url_relation.full_version)
     else
       render :new
     end
--- app/models/url_relation.rb
+++ app/models/url_relation.rb
@@ -2,13 +2,18 @@ class UrlRelation < ApplicationRecord
   validates :short_version, presence: true
   validates :full_version, url: { no_local: true }
 
-  before_validation :generate_short_version
+  before_validation :get_short_version, on: :create
 
   private
 
-  def generate_short_version
-    return if short_version.present? || full_version.blank?
+  def get_short_version
+    return if full_version.blank?
 
-    self.short_version = Digest::MD5.hexdigest(full_version)
+    self.short_version ||= generate_short_version_hash
   end
+
+  def generate_short_version_hash
+    Digest::MD5.hexdigest(full_version)
+  end
+
 end
